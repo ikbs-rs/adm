b0be84a46a4c72c4fc264eb488922304dbf7b6d0
ikbs-rs <ikbs.rs@gmail.com>
2023-11-08 03:37:58 +0100

    ...

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(cors());
 
 dotenv.config()
 app.use((req, res, next) => { 
+  console.log("*******webDomen**********", webDomen)
   next();
 });
 const httpPort = process.env.APP_PORT || 80; // HTTP port
@@ -27,6 +28,7 @@ const credentials = { key: privateKey, cert: certificate };
 
  //Dodajte middleware za omogućavanje CORS
  app.use((req, res, next) => {
+  console.log("*******webDomen**********", webDomen)
   const allowedOrigins = [webDomen, '*.ems.local'];
   const origin = req.headers.origin;
 
